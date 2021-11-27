e655d66d6f5e228b3799c7a5e5560044301dbb38
poteboy <keitatkins4@gmail.com>
2021-11-28 01:24:24 +0900

    install graphql

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import {ApolloServer, gql} from "apollo-server-cloud-functions";
+import {server} from "./test";
 
 admin.initializeApp();
 
 exports.createNewUser = functions.auth.user().onCreate((user) => {
-  const { uid, email, displayName } = user;
+  const {uid, email, displayName} = user;
   const account = {
     uid,
     email,
@@ -18,44 +20,46 @@ exports.createNewUser = functions.auth.user().onCreate((user) => {
 });
 
 exports.deleteUser = functions.auth.user().onDelete((user) => {
-  const { uid } = user;
+  const {uid} = user;
 
   functions.logger.log("user deleted", user.email, uid);
   return admin.firestore().collection("users").doc(uid).delete();
 });
 
 exports.updateUser = functions.firestore
-  .document("incoming_user_changes/{docId}")
-  .onCreate((snap) => {
-    const original = snap.data();
-    const { uid, displayName, birthday, age, weight } = original;
-    functions.logger.log(
-      "incoming user changes",
-      uid,
-      displayName,
-      birthday,
-      age,
-      weight
-    );
+    .document("incoming_user_changes/{docId}")
+    .onCreate((snap) => {
+      const original = snap.data();
+      const {uid, displayName, birthday, age, weight} = original;
+      functions.logger.log(
+          "incoming user changes",
+          uid,
+          displayName,
+          birthday,
+          age,
+          weight
+      );
 
-    return admin.firestore().collection("users").doc(uid).update({
-      displayName,
-      birthday,
-      age,
-      weight,
+      return admin.firestore().collection("users").doc(uid).update({
+        displayName,
+        birthday,
+        age,
+        weight,
+      });
     });
-  });
 
 exports.deleteUserChanges = functions.firestore
-  .document("users/{userId}")
-  .onUpdate((snap, context) => {
-    return admin
-      .database()
-      .ref("/other")
-      .orderByChild("id")
-      .equalTo(context.params.pushId)
-      .once("value")
-      .then((snapshot) => {});
+    .document("users/{userId}")
+    .onUpdate((snap, context) => {
+      // return admin
+      //     .database()
+      //     .ref("/other")
+      //     .orderByChild("id")
+      //     .equalTo(context.params.pushId)
+      //     .once("value")
+      //     .then((snapshot) => {});
 
-    return admin.firestore().collection("incoming_user_changes").doc();
-  });
+      return admin.firestore().collection("incoming_user_changes").doc();
+    });
+
+exports.graphql = functions.https.onRequest(server.createHandler());
