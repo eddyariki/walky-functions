491349762bf321cce06d6e114ff6dcfb0353ad15
eddyariki <edoueda@gmail.com>
2021-11-29 16:32:03 +0900

    update based on docId

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -32,24 +32,41 @@ exports.deleteUser = functions.auth.user().onDelete(async (user) => {
 
 exports.updateUser = functions.firestore
     .document("incoming_user_changes/{docId}")
-    .onCreate((snap) => {
+    .onCreate(async (snap, context) => {
       const original = snap.data();
-      const {uid, displayName, birthday, age, weight} = original;
-      functions.logger.log(
-          "incoming user changes",
-          uid,
+      const docId = context.params.docId;
+      const ref = await admin.firestore().collection("users").doc(docId).get();
+      if (!ref.exists) {
+        functions.logger.log("user does not exist");
+
+        return admin
+            .firestore()
+            .collection("incoming_user_changes")
+            .doc(docId)
+            .delete();
+      } else {
+        const {displayName, birthday, age, weight} = original;
+        functions.logger.log(
+            "incoming user changes",
+            docId,
+            displayName,
+            birthday,
+            age,
+            weight
+        );
+
+        await admin.firestore().collection("users").doc(docId).update({
           displayName,
           birthday,
           age,
-          weight
-      );
-
-      return admin.firestore().collection("users").doc(uid).update({
-        displayName,
-        birthday,
-        age,
-        weight,
-      });
+          weight,
+        });
+        return admin
+            .firestore()
+            .collection("incoming_user_changes")
+            .doc(docId)
+            .delete();
+      }
     });
 
 exports.deleteUserChanges = functions.firestore
