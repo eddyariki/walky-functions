1e72eea35f966d959d002a56881a88a5fab5a37e
eddyariki <edoueda@gmail.com>
2021-12-11 11:23:31 +0900

    merging with main

---
+++ tests/test.js
@@ -0,0 +1,57 @@
+const assert = require("assert");
+const firebase = require("@firebase/testing");
+
+const MY_PROJECT_ID = "pedometer-develop";
+const myId = "user_uid_abc";
+const theirId = "user_uid_dxy";
+const myAuth = { uid: myId, email: "abc@gmail.com" };
+
+function getFirestore(auth) {
+  return firebase
+    .initializeTestApp({ projectId: MY_PROJECT_ID, auth: auth })
+    .firestore();
+}
+
+function getAdminFirestore() {
+  return firebase.initializeAdminApp({ projectId: MY_PROJECT_ID }).firestore();
+}
+
+beforeEach(async () => {
+  await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+});
+
+describe("Our social app", () => {
+  it("Can read items in the read-only users collection", async () => {
+    const db = getFirestore(null);
+    const testDoc = db.collection("users").doc("testDoc");
+    await firebase.assertSucceeds(testDoc.get());
+  });
+
+  it("Can't write items in the read-only users collection", async () => {
+    const db = getFirestore(null);
+    const testDoc = db.collection("users").doc("testDoc");
+    await firebase.assertFails(testDoc.set({ foo: "bar" }));
+  });
+
+  it("Can write to an incoming_user_changes document with the same ID as our user", async () => {
+    const db = getFirestore(myAuth);
+    const testDoc = db.collection("incoming_user_changes").doc(myId);
+    await firebase.assertSucceeds(testDoc.set({ uid: myId }));
+  });
+
+  it("Can't write to an incoming_user_changes document with a different ID as our user", async () => {
+    const db = getFirestore(myAuth);
+    const testDoc = db.collection("incoming_user_changes").doc(theirId);
+    await firebase.assertFails(testDoc.set({ uid: theirId }));
+  });
+
+  it("Can't write to an incoming_user_changes document without login", async () => {
+    const db = getFirestore(null);
+    const testDoc = db.collection("incoming_user_changes").doc(myId);
+    await firebase.assertFails(testDoc.set({ uid: myId }));
+  });
+});
+
+after(async () => {
+  await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+});
