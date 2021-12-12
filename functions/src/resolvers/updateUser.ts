f9624f37e8fc3f06e047736466f7f52ce376a566
eddyariki <edoueda@gmail.com>
2021-12-13 00:50:14 +0900

    changed registerUser to updateUser

--- functions/src/resolvers/updateUser.ts
+++ functions/src/resolvers/updateUser.ts
@@ -4,13 +4,27 @@ import {AuthenticationError, ExpressContext} from "apollo-server-express";
 
 export const updateUser = async (
     _: null,
-    {uid, name}: {uid: string, name: string},
+    {
+      uid,
+      name,
+      age,
+      weight,
+    }
+      :
+      {
+        uid: string,
+        name?: string,
+        age? : number,
+        weight?: number
+      },
     context: ExpressContext
 ):Promise<FirebaseFirestore.DocumentData | undefined> => {
   const reqUser = await validateFirebaseIdToken(context);
   if (uid === reqUser.uid) {
     await admin.firestore().collection("users").doc(uid).set({
-      name: name,
+      name,
+      age,
+      weight,
     });
     const userRef = await admin.firestore().doc(`users/${uid}`).get();
     const user = await userRef.data();
