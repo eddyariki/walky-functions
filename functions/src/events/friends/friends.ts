69540f35ce619978a9267608770a82bf58280492
eddyariki <edoueda@gmail.com>
2021-12-22 17:48:04 +0900

    subcollection friendsList

--- functions/src/events/friends/friends.ts
+++ functions/src/events/friends/friends.ts
@@ -9,6 +9,58 @@ import * as admin from "firebase-admin";
 
 
 // make rules for this so only proper users can create it
+// export const acceptFriendEvent = functions
+//     .firestore
+//     .document("incoming_friend_accept/{docId}")
+//     .onCreate(async (snap, context)=>{
+//       const values = snap.data();
+//       const docId = context.params.docId;
+//       functions.logger.log(docId);
+//       functions.logger.log(values);
+//       //   const doc = await userFriendsRef.get();
+//       // check to make sure friend has actually requested to be added
+//       const friendsRef = await admin.firestore().collection("users")
+//           .doc(values.uid)
+//           .collection("friends")
+//           .doc(values.uid)
+//           .get();
+//       if (friendsRef?.data()?.pendingFriends.includes(values.friendUid)) {
+//         // change self's friendlist
+//         await admin.firestore().collection("users")
+//             .doc(values.uid)
+//             .collection("friends")
+//             .doc(values.uid)
+//             .set({
+//               friendsList: admin
+//                   .firestore
+//                   .FieldValue
+//                   .arrayUnion(values.friendUid),
+//               pendingFriends: admin
+//                   .firestore
+//                   .FieldValue
+//                   .arrayRemove(values.friendUid),
+//             });
+
+//         await admin.firestore().collection("users")
+//             .doc(values.friendUid).collection("friends")
+//             .doc(values.friendUid).set({
+//               friendsList: admin
+//                   .firestore
+//                   .FieldValue
+//                   .arrayUnion(values.uid),
+//             });
+//         return admin
+//             .firestore()
+//             .collection("incoming_friend_accept")
+//             .doc(docId)
+//             .delete();
+//       } else {
+//         functions.logger.error("friend not in pendingFriends");
+//         return;
+//       }
+//     });
+
+
 export const acceptFriendEvent = functions
     .firestore
     .document("incoming_friend_accept/{docId}")
@@ -21,33 +73,28 @@ export const acceptFriendEvent = functions
       // check to make sure friend has actually requested to be added
       const friendsRef = await admin.firestore().collection("users")
           .doc(values.uid)
-          .collection("friends")
-          .doc(values.uid)
+          .collection("pending_friends")
+          .doc(values.friendUid)
           .get();
-      if (friendsRef?.data()?.pendingFriends.includes(values.friendUid)) {
+      if (friendsRef) {
         // change self's friendlist
         await admin.firestore().collection("users")
             .doc(values.uid)
-            .collection("friends")
+            .collection("pending_friends")
+            .doc(values.friendUid)
+            .delete();
+        await admin.firestore().collection("users")
             .doc(values.uid)
+            .collection("friends")
+            .doc(values.friendUid)
             .set({
-              friendsList: admin
-                  .firestore
-                  .FieldValue
-                  .arrayUnion(values.friendUid),
-              pendingFriends: admin
-                  .firestore
-                  .FieldValue
-                  .arrayRemove(values.friendUid),
+              friendUid: values.friendUid,
             });
 
         await admin.firestore().collection("users")
             .doc(values.friendUid).collection("friends")
-            .doc(values.friendUid).set({
-              friendsList: admin
-                  .firestore
-                  .FieldValue
-                  .arrayUnion(values.uid),
+            .doc(values.uid).set({
+              friendUid: values.uid,
             });
         return admin
             .firestore()
--- functions/src/resolvers/rejectFriend.ts
+++ functions/src/resolvers/rejectFriend.ts
@@ -2,15 +2,43 @@ import * as admin from "firebase-admin";
 import {validateFirebaseIdToken} from "../helpers";
 import {ExpressContext} from "apollo-server-express";
 
+// export const rejectFriend = async (
+//     _: null,
+//     {
+//       friendUid, // friend's uid
+//     }
+//       :
+//       {
+//         friendUid: string,
+//       },
+//     context: ExpressContext
+// ):Promise<string | undefined> => {
+//   const reqUser = await validateFirebaseIdToken(context);
+
+//   await admin
+//       .firestore()
+//       .collection("users")
+//       .doc(reqUser.uid)
+//       .collection("friends")
+//       .doc(reqUser.uid)
+//       .set({
+//         pendingFriends: admin.firestore
+//             .FieldValue
+//             .arrayRemove(friendUid),
+//       });
+//   return friendUid;
+// };
+
+
 export const rejectFriend = async (
     _: null,
     {
       friendUid, // friend's uid
     }
-      :
-      {
-        friendUid: string,
-      },
+    :
+    {
+      friendUid: string,
+    },
     context: ExpressContext
 ):Promise<string | undefined> => {
   const reqUser = await validateFirebaseIdToken(context);
@@ -19,12 +47,8 @@ export const rejectFriend = async (
       .firestore()
       .collection("users")
       .doc(reqUser.uid)
-      .collection("friends")
-      .doc(reqUser.uid)
-      .set({
-        pendingFriends: admin.firestore
-            .FieldValue
-            .arrayRemove(friendUid),
-      });
+      .collection("pending_friends")
+      .doc(friendUid)
+      .delete();
   return friendUid;
 };
